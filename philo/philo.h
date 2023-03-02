29f5330787b1e2d3a6fa0a3afc8f2ab453cc4f7b
Davut UZUN <duzun@Davut-iMac.local>
2023-03-03 00:33:55 +0300

    Update..

--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: duzun <davut@uzun.ist>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 14:41:04 by duzun             #+#    #+#             */
-/*   Updated: 2023/02/28 14:42:20 by duzun            ###   ########.fr       */
+/*   Updated: 2023/03/03 00:19:38 by duzun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,33 @@
 # define PHILO_H
 
 # include "../libft/libft.h"
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <pthread.h>
+# include <sys/time.h>
+
+typedef struct s_game
+{
+	int				number_of_philosophers;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				philosopher_must_eat ;
+	int				philosopher_total_ate;
+	unsigned long	time;
+}	t_game;
+
+typedef struct s_philo
+{
+	int				ate;
+	int				id;
+	unsigned long	last_meal;
+	t_game			*gms;
+	pthread_mutex_t	*mutex;
+	pthread_mutex_t	*print;
+}	t_philo;
+
 
 #endif
